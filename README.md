0935b0aec6fc9906b33971e5e7e5c2f8db64d27a
Syed Ali Raza Bokhari <isyedaliraza@gmail.com>
2022-08-02 15:02:58 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Script for Installing Odoo 15.0
+
+## Usage
+
+- Download and move the script to the path where you want to install odoo.
+- Open the script in a text editor and insert the GITHUB_PAT variable value. You can create your own Github Personal Access Token and put it here.
+- Make sure the script is executeable.
+- Run the script.
---
+++ install_odoo.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+GITHUB_PAT=""
+
+fetch_sources() {
+	echo "Fetching the sources"
+	git clone "https://github.com/odoo/odoo.git" --depth 1 -b 15.0
+	git clone "https://isyedaliraza:$GITHUB_PAT@github.com/odoo/enterprise.git" --depth 1 -b 15.0
+	mv ./enterprise/* ./odoo/addons
+	rm -rf ./enterprise
+	echo "Fetched the sources successfully"
+}
+
+install_python() {
+	echo "Installing Python 3.7"
+	sudo apt install software-properties-common -y
+	sudo add-apt-repository ppa:deadsnakes/ppa -y
+	sudo apt install python3.7 python3.7-distutils python3.7-dev python3-pip -y
+	echo -e "$(python3.7 --version) installed successfully\n"
+	echo "Installing python dependencies"
+	sudo apt install libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev \
+		libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev \
+		liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev -y
+	echo "Python dependencies installed successfully"
+}
+
+install_postgresql() {
+	echo "Installing PostgreSQL"
+	sudo apt install postgresql postgresql-client -y
+	cd /tmp
+	sudo -u postgres createuser -s odoo
+	createdb odoo
+	cd - > /dev/null	
+	echo "Installed PostgreSQL successfully"
+}
+
+install_odoo_dependencies() {
+	echo "Installing odoo dependencies"
+	cd ./odoo
+	python3.7 -m pip install setuptools wheel
+	python3.7 -m pip install -r requirements.txt
+	cd ..
+	echo "Odoo dependencies installed successfully"
+}
+
+install_node_and_rtlcss() {
+	echo "Installing nodejs and dependencies"
+	sudo apt install nodejs npm -y
+	sudo npm install -g rtlcss
+	echo "Installed nodejs and dependencies successfully"
+}
+
+fetch_sources
+install_python
+install_postgresql
+install_odoo_dependencies
+install_node_and_rtlcss
