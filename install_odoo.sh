faa2e3daff3cefa61632b3e6af888b7460bb6248
Syed Ali Raza Bokhari <32142347+isyedaliraza@users.noreply.github.com>
2022-08-03 16:54:59 +0400

    Update install_odoo.sh

--- install_odoo.sh
+++ install_odoo.sh
@@ -24,7 +24,7 @@ install_dependencies() {
 install_odoo() {
 	wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 	echo "deb http://nightly.odoo.com/15.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
-	apt install odoo -y	
+	apt update && apt install odoo -y	
 }
 
 fetch_enterprise_addons() {
