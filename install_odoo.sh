049b65ff9b0b64dad95abc9a35cd8e7659e6896a
Syed Ali Raza Bokhari <32142347+isyedaliraza@users.noreply.github.com>
2022-08-03 12:00:52 +0400

    Update install_odoo.sh

--- install_odoo.sh
+++ install_odoo.sh
@@ -21,8 +21,8 @@ fetch_sources() {
 install_python() {
 	sudo apt install software-properties-common -y
 	sudo add-apt-repository ppa:deadsnakes/ppa -y
-	sudo apt install python3.7 python3.7-distutils python3.7-dev python3-pip -y
-	sudo apt install libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev \
+	sudo apt install python3.7 python3.7-distutils python3.7-dev \
+		python3-pip libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev \
 		libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev \
 		liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev -y
 }
